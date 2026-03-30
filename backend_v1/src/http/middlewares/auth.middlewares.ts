c330c5dedc8a3ffdefee5d7a48b96bc3ccb4c600
nikshrma <nikshrma2006@gmail.com>
2026-03-30 19:23:46 +0530

    auth middleware

--- backend_v1/src/http/middlewares/auth.middlewares.ts
+++ backend_v1/src/http/middlewares/auth.middlewares.ts
@@ -1,3 +1,31 @@
-export default async function authMiddleware(){
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
+export default function authMiddleware(req: Request,res: Response,next: NextFunction) {
+    const token = req.cookies?.token;
+    if (!token) {
+        return res.status(401).json({
+            message: "No token provided",
+        });
+    }
+    const JWT_SECRET = process.env.JWT_PASS;
+    if (!JWT_SECRET) {
+        throw new Error("JWT_PASS is not defined");
+    }
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        if (typeof decoded === "string" || !("userId" in decoded)) {
+            return res.status(401).json({
+                message: "Invalid token",
+            });
+        }
+
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: "Invalid token",
+        });
+    }
 }
