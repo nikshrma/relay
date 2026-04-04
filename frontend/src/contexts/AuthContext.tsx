a5f882f2cfcb2c6162407dfc11644aa06d37470c
nikshrma <nikshrma2006@gmail.com>
2026-04-04 21:33:27 +0530

    added import aliases

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,1 +1,11 @@
+import type { User } from "@/types";
 
+
+interface AuthContextType{
+    user:User | null;
+    isLoading: boolean;
+    isAuthenticated: boolean;
+    signin: (number:string, password:string)=>Promise<void>;
+    signup: (number:string, password:string , name:string)=>Promise<void>
+    logout:()=>void
+}
