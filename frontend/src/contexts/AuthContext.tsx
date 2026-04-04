80319094ea3a7fe14dc40506aabd47a6c7c5fec7
nikshrma <nikshrma2006@gmail.com>
2026-04-04 22:41:25 +0530

    added AuthContext

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,4 +1,6 @@
+import api from "@/api/api";
 import type { User } from "@/types";
+import React,{ createContext, useContext, useEffect, useState } from "react";
 
 
 interface AuthContextType{
@@ -8,4 +10,47 @@ interface AuthContextType{
     signin: (number:string, password:string)=>Promise<void>;
     signup: (number:string, password:string , name:string)=>Promise<void>
     logout:()=>void
+}
+
+const AuthContext = createContext< AuthContextType | undefined >(undefined)
+export function AuthProvider({children}:{children: React.ReactNode}){
+    const [user , setUser] = useState<User | null>(null);
+    const [isLoading , setIsLoading] = useState(true);
+
+    useEffect(()=>{
+        api.me()
+        .then(setUser)
+        .catch(()=>setUser(null))
+        .finally(()=>setIsLoading(false))
+    },[])
+
+    const signup= async(number:string , password:string, name:string)=>{
+        await api.signup({name,number,password});
+        const currentUser = await api.me();
+        setUser(currentUser);
+    }
+    const signin= async(number:string , password:string)=>{
+        await api.signin({number,password});
+        const currentUser = await api.me();
+        setUser(currentUser);
+    }
+    const logout = async()=>{
+        await api.logout();
+        setUser(null);
+    }
+    return (
+    <AuthContext.Provider
+      value={{ user, isLoading, isAuthenticated: !!user, signin, signup, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth(){
+    const context = useContext(AuthContext);
+    if(!context){
+       throw new Error("useAuth must be used within provider");
+    }
+    return context;
 }
