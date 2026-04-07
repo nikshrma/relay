67de83cc9f35287ce6d3fbd5e862b57c3e9b5c8e
nikshrma <nikshrma2006@gmail.com>
2026-04-07 13:08:23 +0530

    refactor

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,61 @@
+import axios, { type AxiosInstance } from "axios";
+import type { User, ApiMessage, Message, SignupPayload, SigninPayload } from "@/types";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+class APIClient {
+    private static instance: APIClient;
+    private client: AxiosInstance;
+    private constructor() {
+        this.client = axios.create({
+            baseURL: API_BASE_URL,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            withCredentials: true
+        });
+        this.client.interceptors.response.use((response)=> response,(error)=>{
+            if (error.response?.status===401){
+                //TODO: redirect to login page
+            }
+            return Promise.reject(error);
+        })
+    }
+    public static getInstance(): APIClient {
+        if (!APIClient.instance) {
+            APIClient.instance = new APIClient();
+        }
+        return APIClient.instance;
+    }
+    async signup(payload: SignupPayload): Promise<ApiMessage> {
+        const { data } = await this.client.post<ApiMessage>('/signup', payload);
+        return data;
+    }
+    async signin(payload: SigninPayload): Promise<ApiMessage> {
+        const { data } = await this.client.post<ApiMessage>('/signin', payload);
+        return data;
+    }
+    async getMessages(userId: string): Promise<Message[]> {
+        const { data } = await this.client.get<{ messages: Message[] }>('/messages', {
+            params: {
+                userId
+            }
+        });
+        return data.messages;
+    }
+    async getUsers(): Promise<User[]> {
+        const { data } = await this.client.get<{ users: User[] }>('/users');
+        return data.users;
+    }
+    async logout(): Promise<ApiMessage>{
+        const { data } = await this.client.post<ApiMessage>('/logout');
+        return data;
+    }
+    async me(): Promise<User>{
+        const { data } = await this.client.get<User>('/me');
+        return data;
+    }
+}
+
+const api = APIClient.getInstance();
+export default api;
