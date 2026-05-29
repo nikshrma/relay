7254f7d9984155bfa6b01f82fb7c8375ec5383b7
nikshrma <nikshrma2006@gmail.com>
2026-05-29 17:38:28 +0530

    added delivered status

--- backend_v1/src/ws/handlers/message.handler.ts
+++ backend_v1/src/ws/handlers/message.handler.ts
@@ -5,83 +5,128 @@ import { prisma } from "../../lib/db.js";
 import { sockets } from "../store.js";
 
 interface BasePayload {
-    to:string;
+  to: string;
 }
 
-interface MessagePayload extends BasePayload{
-    content:string;
+interface MessagePayload extends BasePayload {
+  content: string;
 }
 
 interface TypingPayload extends BasePayload {}
 
 export type WsMessage =
-    | {
-          type: "send_message";
-          payload: MessagePayload;
-      }
-    | {
-          type: "typing";
-          payload: TypingPayload;
-      }
-    | {
-          type: "stop_typing";
-          payload: TypingPayload;
-      };
+  | {
+      type: "send_message";
+      payload: MessagePayload;
+    }
+  | {
+      type: "typing";
+      payload: TypingPayload;
+    }
+  | {
+      type: "stop_typing";
+      payload: TypingPayload;
+    };
 
 export function extractUserId(req: IncomingMessage): string | null {
-    const cookies = cookie.parse(req.headers.cookie ?? "");
-    const token = cookies.token;
-    if (!token) return null;
+  const cookies = cookie.parse(req.headers.cookie ?? "");
+  const token = cookies.token;
+  if (!token) return null;
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-        if (!decoded || typeof decoded !== "object" || !("id" in decoded)) {
-            return null;
-        }
-        return decoded.id as string;
-    } catch {
-        return null;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    if (!decoded || typeof decoded !== "object" || !("id" in decoded)) {
+      return null;
     }
+    return decoded.id as string;
+  } catch {
+    return null;
+  }
 }
 
 async function saveMessage(to: string, from: string, content: string) {
-    return prisma.message.create({
-        data: { senderId: from, receiverId: to, content },
+  return prisma.message.create({
+    data: { senderId: from, receiverId: to, content },
+  });
+}
+
+export async function sendMessage(
+  senderId: string,
+  msg: Extract<WsMessage, { type: "send_message" }>,
+) {
+  const { to, content } = msg.payload;
+  if (!to || !content) {
+    throw new Error("Missing required fields: 'to' and 'content'");
+  }
+
+  await saveMessage(to, senderId, content);
+
+  const sender = await prisma.user.findUnique({ where: { id: senderId } });
+  sockets.sendToUser(to, {
+    type: "receive_message",
+    payload: { from: senderId, name: sender?.name, content },
+  });
+}
+export async function sendTyping(
+  senderId: string,
+  msg: Extract<WsMessage, { type: "typing" }>,
+) {
+  const { to } = msg.payload;
+  if (!to) {
+    throw new Error("Missing field: to");
+  }
+  sockets.sendToUser(to, {
+    type: "typing",
+    payload: { userId: senderId },
+  });
+}
+export async function sendStopTyping(
+  senderId: string,
+  msg: Extract<WsMessage, { type: "stop_typing" }>,
+) {
+  const { to } = msg.payload;
+  if (!to) {
+    throw new Error("Missing field: to");
+  }
+  sockets.sendToUser(to, {
+    type: "stop_typing",
+    payload: { userId: senderId },
+  });
+}
+export async function markMessagesAsDelivered(receiverId: string) {
+  const updateUsers = await prisma.message.findMany({
+    where: {
+      receiverId,
+      deliveredAt: null,
+    },
+    select: {
+      id: true,
+      senderId: true,
+    },
+  });
+  await prisma.message.updateMany({
+    where: {
+      receiverId,
+      deliveredAt: null,
+    },
+    data: {
+      deliveredAt: new Date(),
+    },
+  });
+  const senderMessagesMap = new Map<string, string[]>();
+
+  for (const msg of updateUsers) {
+    const msgIds = senderMessagesMap.get(msg.senderId) ?? [];
+    msgIds.push(msg.id);
+    senderMessagesMap.set(msg.senderId, msgIds);
+  }
+  for (const [senderId, messageIds] of senderMessagesMap) {
+    sockets.sendToUser(senderId, {
+      type: "delivered_messages",
+      payload: {
+        messageIds: messageIds,
+      },
     });
+  }
 }
 
-export async function sendMessage(senderId: string, msg: Extract<WsMessage , {type:"send_message"}>) {
-    const { to, content } = msg.payload;
-    if (!to || !content) {
-        throw new Error("Missing required fields: 'to' and 'content'");
-    }
-
-    await saveMessage(to, senderId, content);
-
-
-    const sender = await prisma.user.findUnique({ where: { id: senderId } });
-    sockets.sendToUser(to,{
-        type: "receive_message",
-        payload: { from: senderId, name: sender?.name, content },
-    })
-}
-export async function sendTyping(senderId:string , msg:Extract<WsMessage , {type:"typing"}>){
-    const {to} = msg.payload;
-    if(!to){
-        throw new Error("Missing field: to")
-    }
-    sockets.sendToUser(to , {
-        type:"typing",
-        payload:{userId:senderId}
-    })
-}
-export async function sendStopTyping(senderId:string , msg:Extract<WsMessage , {type:"stop_typing"}>){
-    const {to} = msg.payload;
-    if(!to){
-        throw new Error("Missing field: to")
-    }
-    sockets.sendToUser(to , {
-        type:"stop_typing",
-        payload:{userId:senderId}
-    })
-}
--- backend_v1/src/ws/socket.ts
+++ backend_v1/src/ws/socket.ts
@@ -1,70 +1,86 @@
 import { WebSocketServer, type WebSocket } from "ws";
 import type { Server as HttpServer } from "http";
-import { extractUserId, sendMessage, sendStopTyping, sendTyping, type WsMessage } from "./handlers/message.handler.js";
+import {
+  extractUserId,
+  markMessagesAsDelivered,
+  sendMessage,
+  sendStopTyping,
+  sendTyping,
+  type WsMessage,
+} from "./handlers/message.handler.js";
 import { sockets } from "./store.js";
 
 export function initWebSocketServer(server: HttpServer) {
-    const wss = new WebSocketServer({ server });
+  const wss = new WebSocketServer({ server });
 
-    wss.on("connection", (ws: WebSocket, request) => {
-        ws.on("error", console.error);
+  wss.on("connection", (ws: WebSocket, request) => {
+    ws.on("error", console.error);
 
-        const id = extractUserId(request);
-        if (!id) {
-            ws.close(4001, "Unauthorized");
-            return;
-        }
+    const id = extractUserId(request);
+    if (!id) {
+      ws.close(4001, "Unauthorized");
+      return;
+    }
 
-        sockets.addUser(id, ws);
+    sockets.addUser(id, ws);
+    markMessagesAsDelivered(id);
+    ws.send(
+      JSON.stringify({
+        type: "online-users",
+        payload: {
+          users: [...sockets.getOnlineUsers()],
+        },
+      }),
+    );
 
-        ws.send(JSON.stringify({
-            type:"online-users",
-            payload:{
-                users:[...sockets.getOnlineUsers()]
-            }
-        }))
-
-        sockets.broadcast({
-            type: "online",
-            payload: {
-                userId:id
-            }
-        })
-
-        ws.on("message", async (data) => {
-            try {
-                const msg: WsMessage = JSON.parse(data.toString());
-                if (!msg.type) return;
-
-                switch (msg.type) {
-                    case "send_message": {
-                        await sendMessage(id, msg);
-                        ws.send(JSON.stringify({ type: "ack", payload: { status: "sent" } }));
-                        break;
-                    }
-                    case "typing":{
-                        await sendTyping(id,msg);
-                        break;
-                    }
-                    case "stop_typing":{
-                        await sendStopTyping(id,msg);
-                        break;
-                    }
-                    default:
-                        break;
-                }
-            } catch (e) {
-                console.error("WS error", e);
-                ws.send(JSON.stringify({ type: "error", payload: { message: "Failed to process message" } }));
-            }
-        });
-
-        ws.on("close", () => {
-            sockets.removeUserSocket(id, ws);
-            sockets.broadcast({
-                type: "offline",
-                payload: { userId: id }
-            });
-        });
+    sockets.broadcast({
+      type: "online",
+      payload: {
+        userId: id,
+      },
     });
+
+    ws.on("message", async (data) => {
+      try {
+        const msg: WsMessage = JSON.parse(data.toString());
+        if (!msg.type) return;
+
+        switch (msg.type) {
+          case "send_message": {
+            await sendMessage(id, msg);
+            ws.send(
+              JSON.stringify({ type: "ack", payload: { status: "sent" } }),
+            );
+            break;
+          }
+          case "typing": {
+            await sendTyping(id, msg);
+            break;
+          }
+          case "stop_typing": {
+            await sendStopTyping(id, msg);
+            break;
+          }
+          default:
+            break;
+        }
+      } catch (e) {
+        console.error("WS error", e);
+        ws.send(
+          JSON.stringify({
+            type: "error",
+            payload: { message: "Failed to process message" },
+          }),
+        );
+      }
+    });
+
+    ws.on("close", () => {
+      sockets.removeUserSocket(id, ws);
+      sockets.broadcast({
+        type: "offline",
+        payload: { userId: id },
+      });
+    });
+  });
 }
