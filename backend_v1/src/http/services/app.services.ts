cff8187283d24e4244eaa53dd30dc437e3a612a5
nikshrma <nikshrma2006@gmail.com>
2026-06-02 00:56:01 +0530

    fix: included timestamps in history fetches

--- backend_v1/src/http/services/app.services.ts
+++ backend_v1/src/http/services/app.services.ts
@@ -1,52 +1,55 @@
 import { prisma } from "../../lib/db.js";
 
-export async function fetchUsers(id: string){
-    const otherUsers = await prisma.user.findMany({
-        where:{
-            id:{
-                not:id
-            }
-        },
-        select:{
-            id:true,
-            name:true,
-            number:true,
-            password:false
-        }
-    })
-    return otherUsers;
+export async function fetchUsers(id: string) {
+  const otherUsers = await prisma.user.findMany({
+    where: {
+      id: {
+        not: id,
+      },
+    },
+    select: {
+      id: true,
+      name: true,
+      number: true,
+      password: false,
+    },
+  });
+  return otherUsers;
 }
 
-export async function fetchMessages(userA: string, userB: string){
-    const messages = await prisma.message.findMany({
-        where:{
-            OR:[
-                {
-                    receiverId:userA,
-                    senderId:userB
-                },
-                {
-                    receiverId:userB,
-                    senderId:userA
-                }
-            ],
+export async function fetchMessages(userA: string, userB: string) {
+  const messages = await prisma.message.findMany({
+    where: {
+      OR: [
+        {
+          receiverId: userA,
+          senderId: userB,
         },
-        select:{
-            content:true,
-            createdAt:true,
-            id:true,
-            senderId:true,
-            receiverId:true,
-            sender:{
-                select:{
-                    name:true,
-                    id:true
-                }
-            }
+        {
+          receiverId: userB,
+          senderId: userA,
         },
-        orderBy:{
-            createdAt:"asc"
-        }
-    })
-    return messages;
+      ],
+    },
+    select: {
+      content: true,
+      createdAt: true,
+      id: true,
+      senderId: true,
+      receiverId: true,
+      deliveredAt: true,
+      readAt: true,
+      sender: {
+        select: {
+          name: true,
+          id: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+  return messages;
 }
+
