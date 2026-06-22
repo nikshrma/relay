a1582cbf2c042d29fb15753ac9e4067bcc50951f
nikshrma <nikshrma2006@gmail.com>
2026-06-22 21:30:21 +0530

    subscriber initialisation function

--- backend_v1/src/index.ts
+++ backend_v1/src/index.ts
@@ -1,6 +1,8 @@
+import { initializeSubscriptions } from "./lib/redis.js";
 import { createRelayServer } from "./server.js";
 
 const server = createRelayServer();
 server.listen(process.env.PORT || 3000, () => {
   console.log("Server running on port 3000");
 });
+await initializeSubscriptions();
